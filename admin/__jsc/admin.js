5bef218bff671ff242a27d2dfc0a3525b3c64244
Rafinha Milare <rafaelmilare72@gmail.com>
2016-11-02 11:38:48 -0200

    Autocomplete multivalores funcionando

--- admin/__jsc/admin.js
+++ admin/__jsc/admin.js
@@ -57,17 +57,26 @@ $(function() {
     });
 });
 
-
 $(function () {
-    
-    
-    $('#txtAreaAtuacao').autocomplete({
+    function split(val) {
+        return val.split(/,\s*/);
+    }
+    function extractLast(term) {
+        return split(term).pop();
+    }
+
+    $('#txtAreaAtuacao').on( "keydown", function( event ) {
+        if ( event.keyCode === $.ui.keyCode.TAB &&
+            $( this ).autocomplete( "instance" ).menu.active ) {
+          event.preventDefault();
+        }
+      }).autocomplete({
         source: function (request, response) {
             $.ajax({
                 url: '__jsc/areaAtuacao.php',
                 dataType: "json",
                 data: {
-                    name_startsWith: request.term,
+                    name_startsWith: extractLast(request.term),
                     type: 'areaatuacao'
                 },
                 success: function (data) {
@@ -80,8 +89,30 @@ $(function () {
                 }
             });
         },
-        autoFocus: true,
-        minLength: 0
+        search: function () {
+            // custom minLength
+            var term = extractLast(this.value);
+            if (term.length < 2) {
+                return false;
+            }
+        },
+        focus: function () {
+            // prevent value inserted on focus
+            return false;
+        },
+        select: function (event, ui) {
+            var terms = split(this.value);
+            // remove the current input
+            terms.pop();
+            // add the selected item
+            terms.push(ui.item.value);
+            // add placeholder to get the comma-and-space at the end
+            terms.push("");
+            this.value = terms.join(", ");
+            return false;
+        }
     });
-    
+
 });
+
+
